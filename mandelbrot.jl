508f6596672fa4490000dee6719cfe65531d283a
Zoe McCarthy <zoemccarthy12@gmail.com>
2020-09-25 19:18:05 -0500

    Video zoom rendering

--- mandelbrot.jl
+++ mandelbrot.jl
@@ -1,4 +1,6 @@
 using FileIO
+using VideoIO
+using StaticArrays
 import Makie
 
 function mandelbrot(z::Complex{T}, numiters::Integer) where {T <: Real}
@@ -26,27 +28,57 @@ function generateranges(numxrange)
     return xrange, yrange
 end
 
-function plotmandelbulb(xrange, yrange, numiters::Integer, savename::AbstractString) #, plot=true, showaxis=true)
+function generatezoomranges(centerx, centery, scalefactor, numyrange, aspectratio=16/9)
+    expscale = 2 ^ -scalefactor
+    yrangeextent = expscale
+    xrangeextent = expscale * aspectratio
+    numxrange = Int(floor(numyrange * aspectratio))
+    xrange = LinRange(centerx - xrangeextent / 2, centerx + xrangeextent / 2, numxrange)
+    yrange = LinRange(centery - yrangeextent / 2, centery + yrangeextent / 2, numyrange)
+    return xrange, yrange
+end
+
+function plotmandelbulb(xrange, yrange, numiters::Integer, plot=true, showaxis=true)
     complexrange = [Complex{Float64}(x, y) for x in xrange, y in yrange]
     mandelbrotupto(z) = 1 - mandelbrot(z, numiters) / numiters
     mandelbrotimg = mandelbrotupto.(complexrange)
 
-    return mandelbrotimg
-    #if plot
-        #scene = Makie.image(xrange, yrange, mandelbrotimg, show_axis=showaxis, scale_plot=false)
-        #return mandelbrotimg, scene
-    #else
-        #return mandelbrotimg
-    #end
+    if plot
+        scene = Makie.image(xrange, yrange, mandelbrotimg, show_axis=showaxis, scale_plot=false)
+        return mandelbrotimg, scene
+    else
+        return mandelbrotimg
+    end
 
 end
 
 function rendermandelbulbpng(xrange, yrange, numiters::Integer, savename::AbstractString) #, plot=true, showaxis=true)
+    mandelbrotimg = rendermandelbulbimg(xrange, yrange, numiters)
+    save(savename, mandelbrotimg)
+end
+
+function rendermandelbulbimg(xrange, yrange, numiters::Integer) #, plot=true, showaxis=true)
     mandelbrotimg = zeros(UInt8, yrange.len, xrange.len)
     for (j, y) in enumerate(yrange), (i, x) in enumerate(xrange)
         mandelxy = 255 * (1 - mandelbrot(Complex{Float64}(x, y), numiters) / numiters)
         mandelbrotimg[j, i] = UInt8(floor(mandelxy))
     end
-    save(savename, mandelbrotimg)
+    return mandelbrotimg
+end
+
+
+function rendermandelbulbvideo(centerx::Number, centery::Number, scalefactorstart::Number,
+     scalefactorend::Number, numiters::Integer, fps::Integer, seconds::Number, height::Integer, aspectratio::Number, savename::AbstractString)
+    time = 0:1/fps:seconds
+    numsteps = length(time)
+    mandels = Array{Array{UInt8, 2}, 1}(undef, numsteps)
+    for (i, t) in enumerate(time)
+        scalefactor = (scalefactorend - scalefactorstart) * (t / seconds) + scalefactorstart 
+        xrange, yrange = generatezoomranges(centerx, centery, scalefactor, height, aspectratio)
+        mandels[i] = rendermandelbulbimg(xrange, yrange, numiters) 
+    end
+    props = [:priv_data => ("cruf"=>"22", "preset"=>"medium")]
+    encodevideo(savename, mandels, framerate=fps, AVCodecContextProperties=props)
+
 end
 
